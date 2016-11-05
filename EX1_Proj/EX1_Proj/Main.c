0abd0cf142043b26b8ae36bcd9f221065b4c4d74
Lior <Lior@DESKTOP-HOME>
2016-11-05 18:08:13 +0200

    Created visual studio c project

---
+++ EX1_Proj/EX1_Proj/Main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getInputFileData(char* fileLocation){}
+
+int writeDataToOutputFile(char* outputData){}
+
+char* callSolver(char *data)
+{
+	char *solution;
+
+	return solution;
+}
+
+char* callChecker(char *data)
+{
+	char *solution;
+
+	return solution;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("lalal");
+	// open the file from argv[1]
+	// parse file to manegable format (without the seperations?, create structures?)
+	// check running mode (argv[0]) and call the relevant function (0=>solver, 1=>checker)
+	// reparse the data into the solutions style
+	// write the reparsed-date into the output file (argv[2])
+	getchar();
+	return 0;
+}

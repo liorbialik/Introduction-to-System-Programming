bb305aeb7013e1d0c7472240185feaa054e4755b
Lior <Lior@DESKTOP-HOME>
2016-11-05 23:16:08 +0200

    beginning - basic main structure

--- EX1_Proj/EX1_Proj/Main.c
+++ EX1_Proj/EX1_Proj/Main.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int getInputFileData(char* fileLocation){}
 
-int writeDataToOutputFile(char* outputData){}
+//char* getInputFileData(char* inputFilName){}
 
-char* callSolver(char *data)
-{
-	char *solution;
+//int writeDataToOutputFile(char* outputData){}
+//
+//char* callSolver(char *data)
+//{
+//	char *solution = 0;
+//
+//	return solution;
+//}
+//
+//char* callChecker(char *data)
+//{
+//	char *solution = 0;
+//
+//	return solution;
+//}
 
-	return solution;
-}
 
-char* callChecker(char *data)
-{
-	char *solution;
 
-	return solution;
-}
+int main(int argc, char *argv[]){
+	
+	FILE *infile;
+	char *runMode, *inputFileName;
+	char* data[11][11] = {0}; // TODO: need to add a #define ROWS/COLS in .h file
 
-int main(int argc, char *argv[])
-{
-	printf("lalal");
+	// TODO: check if the arguments are givent at run time or needed inside the program
+	runMode = argv[1]; 
+	inputFileName = argv[2];
 	// open the file from argv[1]
 	// parse file to manegable format (without the seperations?, create structures?)
 	// check running mode (argv[0]) and call the relevant function (0=>solver, 1=>checker)
+	//switch (runMode)
+	//{
+	//case 0:
+	//	callSolver(data);
+	//	break;
+	//case 1:
+	//	callChecker(data);
+	//	break;
+	//}
 	// reparse the data into the solutions style
 	// write the reparsed-date into the output file (argv[2])
 	getchar();

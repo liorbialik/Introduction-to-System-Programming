3db86912150b7253e8cda40ca4d2ae29fb42b09a
Lior <bialik@halo-analytics.com>
2016-11-13 14:59:33 +0200

    added functionality: - input parser - changed input reading way - added writing output function - added logic to get outputfile name in case not given by user

--- EX1_Proj/EX1_Proj/Main.c
+++ EX1_Proj/EX1_Proj/Main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "errorHandling.h"
+#include <ctype.h>
 
 // using 'extern' makes sure the error variable is the not duplicated but rather the same one across the program //
 extern int errorNumber;
@@ -8,7 +9,7 @@ extern int errorNumber;
 
 FILE getInputFileData(char* inputFilName) {
 	FILE *inputFile;
-	char *openFileErrorStr[26] = "Input File openning failed";
+	char *openFileErrorStr[26] = "Input File openning failed!";
 
 	fopen_s(inputFile, inputFilName, "r");
 	if (inputFile == NULL) {
@@ -19,26 +20,49 @@ FILE getInputFileData(char* inputFilName) {
 
 
 char* readFileDataIntoBufferArray(FILE *fileToRead){
-    long bufferArraySize;
-    char *bufferArray;
-	char *allocationErrorStr[20] = {0};
+    int i = 0 ,bufferArraySize = 81;
+    char *bufferArray[bufferArraySize];
+    char *allocationErrorStr[20] = {0};
+    char charFromInput;
 
-	*allocationErrorStr = "memory alloc fails";
+    allocationErrorStr = "memory alloc fails";
 
     fseek(fileToRead, 0L, SEEK_END); // Seek to the end of the file //
-    bufferArraySize = ftell(fileToRead); // set the size of the buffer to allocate. //
-    if (bufferArray == NULL) {
-        printErrorAndExitProgram(errorNumber, allocationErrorStr);
-    }
+    inputFileLength = ftell(fileToRead); // set the size of the buffer to allocate. //
     rewind(fileToRead); // reset to point to the beginning of the file //
+
+    while((charFromInput = getc(fileToRead)) != EOF){
+        if ((charFromInput == ".") || (charFromInput > "0" && charFromInput < "10"))
+            bufferArraySize[i] = charFromInput;
+        else
+            continue;
+    }
 
     return bufferArray;
 }
 
+char* parseInputBufferIntoMatrix(char *parsedSudokuMatrix, char *inputBufferArray) {
+    int i, j, inputBufferArraySize = 81;
 
-void writeDataToOutputFile(char *outputData, char *outputFileName)
-{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            parsedSudokuMatrix[i][j] = inputBufferArray[i*inputBufferArraySize + j];
+        }
+    }
+}
 
+void writeDataToOutputFile(char *outputData, char *outputFileName) {
+    FILE *outputFile;
+    char *openFileErrorStr[27] = "Output File creation failed!";
+
+    outputFile = fopen_s(outputFileName, "w+");
+    if (outputFile == NULL) {
+        printErrorAndExitProgram(errorNumber, openFileErrorStr);
+    }
+    fputs(outputData, outputFile);
+    fclose(outputFile);
 }
 
 
@@ -47,24 +71,35 @@ int main(int argc, char *argv[]){
 	FILE *inputFile;
 	int runMode = 0;
 	char* inputFileName, outputFileName, inputBufferArray, solution;
-	char *data[11][11] = {0}; // TODO: need to add a #define ROWS/COLS in .h file
+    char *outputFileNameEnding[8] = "_sol.txt", *parsedSudokuMatrix[9][9] = {0}; // TODO: need to add a #define ROWS/COLS in .h file
 
 	runMode = argv[1][0] - '0'; //convert the input running mode into an integer.
 	inputFileName = argv[2];
-    // open the file from argv[1]
+    if (argc < 3){
+        // allocate the size of outputFileName to be inputFileName.length - "" + "_sol" + 1 (for '\0')
+        // inputFileNameLength = strlen(inputFileName);
+        // outputFileName = inputFileName;
+        // outputFileName[inputFileNameLength -1] = '\0';
+        // strcat(outputFileName, outputFileNameEnding)
+        // create the outputFileName = inputFileName - ".txt" + "_sol.txt"
+    }
+    else
+        outputFileName = argv[3];
+
 	inputFile = getInputFileData(inputFileName);
     inputBufferArray = readFileDataIntoBufferArray(inputFile);
 
     // parse file to manegable format (9X9 matrix)
+    parseInputBufferIntoMatrix(parsedSudokuMatrix, inputBufferArray);
 	// check running mode (argv[0]) and call the relevant function (0=>solver, 1=>checker)
 	switch (runMode)
 	{
 	case 0:
-		solution = 'temp';//callSolver(data);
+		solution = 'temp';//callSolver(parsedSudokuMatrix);
 		break;
 
     case 1:
-        solution = 'temp';//callChecker(data);
+        solution = 'temp';//callChecker(parsedSudokuMatrix);
 		break;
 
     default :

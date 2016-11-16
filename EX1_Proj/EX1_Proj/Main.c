20080496ee0f479bf1164f1e973c0be7b9b6b4e7
Lior <Lior@DESKTOP-VP8ENC9>
2016-11-16 11:14:34 +0200

    reverted changes on main.c file and added tmp_main.c

--- EX1_Proj/EX1_Proj/Main.c
+++ EX1_Proj/EX1_Proj/Main.c
@@ -1,48 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Solver.h"
-#include "Checker.h"
-//#include "Common.h"
+#include "errorHandling.h"
 #include <ctype.h>
 
+// using 'extern' makes sure the error variable is the not duplicated but rather the same one across the program //
+extern int errorNumber;
 
-// Platform main() for checking the functions !!!!!!!!!!!!!!!!!!!!!!!!!
-int main()
-{
-	int i, j;
-	char soduko[9][9] = {
-		{ '.','.','1','7','.','6','3','.','.' },
-		{ '.','.','3','4','.','2','8','.','.' },
-		{ '2','.','.','.','.','.','.','4','.' },
-		{ '.','9','.','1','.','3','.','4','.' },
-		{ '1','.','.','.','8','.','.','.','3' },
-		{ '.','4','.','6','.','7','.','9','.' },
-		{ '8','.','.','.','.','.','.','.','.' },
-		{ '.','.','6','2','.','1','4','.','.', },
-		{ '.','.','9','8','.','.','2','.','.' }
-	};
+// functions decelerations:
+FILE getInputFileData(char* inputFilName);
+char* readFileDataIntoBufferArray(FILE *fileToRead);
+char* parseInputBufferIntoMatrix(char *parsedSudokuMatrix, char *inputBufferArray);
+void writeDataToOutputFile(char *outputData, char *outputFileName);
 
-	/* Printing the matrix */
 
-	printf("\n -- sudoku before -- \n");
-	for (i = 0; i < 9; i++)
+int main(int argc, char *argv[]) {
+
+	FILE *inputFile;
+	int runMode = 0;
+	char* inputFileName, outputFileName, inputBufferArray, solution;
+	char *outputFileNameEnding[8] = "_sol.txt", *parsedSudokuMatrix[9][9] = { 0 }; // TODO: need to add a #define ROWS/COLS in .h file
+
+	runMode = argv[1][0] - '0'; //convert the input running mode into an integer.
+	inputFileName = argv[2];
+	if (argc < 3) { // if no output file name was given, use the input file name and add "_sol" to its end
+		outputFileName = argv[2];
+		outputFileName[strlen(inputFileName) - 4] = '\0'; // remove the ".txt" ending from the input string
+		strcat(outputFileName, outputFileNameEnding); // add the correct ending
+	}
+	else
+		outputFileName = argv[3];
+
+	inputFile = getInputFileData(inputFileName);
+	inputBufferArray = readFileDataIntoBufferArray(inputFile);
+
+	parseInputBufferIntoMatrix(parsedSudokuMatrix, inputBufferArray); // parse file to manegable format (9X9 matrix)
+																	  // check running mode (argv[0]) and call the relevant function (0=>solver, 1=>checker)
+	switch (runMode)
 	{
-		for (j = 0; j < 9; j++)
+	case 0:
+		parsedSudokuMatrix = 'temp';//callSolver(parsedSudokuMatrix);
+		break;
+
+	case 1:
+		parsedSudokuMatrix = 'temp';//callChecker(parsedSudokuMatrix);
+		break;
+
+	default:
+		printf("invalid running mode"); // TODO: call invalid argument error and exit
+	}
+
+	writeDataToOutputFile(parsedSudokuMatrix, outputFileName);
+	getchar();
+	return 0;
+}
+
+FILE getInputFileData(char* inputFilName) {
+	FILE *inputFile;
+	char *openFileErrorStr[26] = "Input File openning failed!";
+
+	fopen_s(inputFile, inputFilName, "r");
+	if (inputFile == NULL) {
+		printErrorAndExitProgram(errorNumber, openFileErrorStr);
+	}
+	return *inputFile;
+}
+
+char* readFileDataIntoBufferArray(FILE *fileToRead) {
+	int i = 0, bufferArraySize = 81;
+	char *bufferArray[bufferArraySize];
+	char *allocationErrorStr[20] = { 0 };
+	char charFromInput;
+
+	allocationErrorStr = "memory alloc fails";
+
+	fseek(fileToRead, 0L, SEEK_END); // Seek to the end of the file //
+	inputFileLength = ftell(fileToRead); // set the size of the buffer to allocate. //
+	rewind(fileToRead); // reset to point to the beginning of the file //
+
+	while ((charFromInput = getc(fileToRead)) != EOF) {
+		if ((charFromInput == ".") || (charFromInput > "0" && charFromInput < "10"))
+			bufferArraySize[i] = charFromInput;
+		else
+			continue;
+	}
+
+	return bufferArray;
+}
+
+char* parseInputBufferIntoMatrix(char *parsedSudokuMatrix, char *inputBufferArray) {
+	int i, j, inputBufferArraySize = 81;
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
 		{
-			printf("%c ", soduko[i][j]);
-			printf("\n");
+			parsedSudokuMatrix[i][j] = inputBufferArray[i*inputBufferArraySize + j];
 		}
 	}
+}
 
-	printf("\n -- sudoku after -- \n");
-	callSolver(soduko);
-	for (i = 0; i < 9; i++)
-	{
-		for (j = 0; j < 9; j++)
-			printf("%c ", soduko[i][j]);
-		printf("\n");
+void writeDataToOutputFile(char *outputData, char *outputFileName) {
+	FILE *outputFile;
+	char *openFileErrorStr[27] = "Output File creation failed!";
+
+	outputFile = fopen_s(outputFileName, "w+");
+	if (outputFile == NULL) {
+		printErrorAndExitProgram(errorNumber, openFileErrorStr);
 	}
-
-	return 0;
+	fputs(outputData, outputFile);
+	fclose(outputFile);
 }
 

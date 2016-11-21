11e7dcd7cf5757f41d91548ba2b21c3d371c9988
Lior <Lior@DESKTOP-VP8ENC9>
2016-11-21 15:53:52 +0200

    TestFile.exe: added the following functionality: - getting the name from the input path - creating the output log file name - getting the file's extention - creating a file's handler - getting the file's size
    all checked for bugs

--- ex2/TestFile/main.c
+++ ex2/TestFile/main.c
@@ -1,40 +1,88 @@
-/*
-plan for testFile.exe program
-*/
 
+
+#define _CRT_SECURE_NO_DEPRECATE // avoid getting errors for '_s functions'
 #include <stdio.h>
-#include <windows.h>
+#include <Windows.h>
 #include <stdlib.h>
 #include <string.h>
+#include <io.h>
+
+char *getFileName(char *path);
+char *getFileExtention(char *filename);
+char *getOutputFileName(char *fileName, char *outputFileNameEnding);
 
 int main(int argc, char *argv[]) {
-	char *filePath = NULL, *outputLogFilePath = NULL;
+	char *filePath = NULL, *outputLogFilePath = NULL, *outputLogFileName = NULL, *fileName = NULL, *fileExtention = NULL, *outputFileNameEnding = { "_log.txt" };
+	FILE *inputFile = NULL;
 	HANDLE fileHandler;
-	FILETIME fileCreationTime;
-	SYSTEMTIME stUTC, stLocal;
-	
-	filePath = argv[1];
+	DWORD fileSize = 0;
+//	FILETIME fileCreationTime;
+//	SYSTEMTIME stUTC, stLocal;
+//	
+	filePath = argv[1]; 
 	outputLogFilePath = argv[2];
+	fileName = getFileName(filePath);
+	fileExtention = getFileExtention(fileName);
+	outputLogFileName = getOutputFileName(fileName, outputFileNameEnding);
 
-	fileHandler = CreateFile(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (fileHandler == INVALID_HANDLE_VALUE)
-	{
-		printf("Could not open file, error %ul\n", GetLastError());
-		return -1;
+	inputFile = fopen(fileName, "r");
+	if (inputFile == NULL) {
+		printf("Input File openning failed!");
+		exit(1);
 	}
 
+	fileHandler = (HANDLE)_get_osfhandle(_fileno(inputFile));
+	fileSize = GetFileSize(fileHandler, NULL);
+
+	printf("%s size is %d bytes and file type is %d\n", fileName, fileSize);
+
+//	fileHandler = CreateFile(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+//	if (fileHandler == INVALID_HANDLE_VALUE)
+//	{
+//		printf("Could not open file, error %ul\n", GetLastError());
+//		return -1;
+//	}
+	getchar();
+	free(outputLogFileName);
+	return 0;
+}
+
+
+char *getFileName(char *path)
+{
+	char *filename = strrchr(path, '\\');
+	if (filename == NULL)
+		filename = path;
+	else
+		filename++;
+	return filename;
+}
+
+
+char *getOutputFileName(char *fileName, char *outputFileNameEnding) {
+	size_t outputFileNameLength = 0;
+	char *outputFileName = NULL;
+
+	outputFileNameLength = strlen(fileName) + strlen(outputFileNameEnding) - 4 + 1; // '-4' deducts the '.txt' ending since it appearse twice. '+1' accounts for the '\0' ending
+	outputFileName = malloc(sizeof(char) * outputFileNameLength); // the '4' stands for the shared ".txt" 
+	if (outputFileName == NULL) {
+		printf("outputFileName allocation faild.");
+		exit(1);
+	}
+	strcpy(outputFileName, fileName);
+	outputFileName[strlen(outputFileName) - 4] = '\0'; // mark the end of the precious string before the '.txt' ending
+	strcat(outputFileName, outputFileNameEnding); // add the correct ending
+	return outputFileName;
 }
 
 // Test 1:
-char *checkFileNameEnding(char *filename){
+char *getFileExtention(char *filename) {
 	Sleep(10);
-	char *dot = NULL;
-	*dot = strrchr(filename, '.');
-	if (!dot || dot == filename) 
-		return "";
-	return dot + 1;
+	char *dotLocationInFileName = strrchr(filename, '.');
+	if (!dotLocationInFileName || dotLocationInFileName == filename)
+		return "No";
+	return dotLocationInFileName + 1;
 }
-
 
 // Test 2:
 // char* checkFileSize(){
---
+++ ex2/TestManager/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	//	char *filePath = NULL, *outputLogFilePath = NULL;
+	//	HANDLE fileHandler;
+	//	FILETIME fileCreationTime;
+	//	SYSTEMTIME stUTC, stLocal;
+	//	
+	//	filePath = argv[1];
+	//	outputLogFilePath = argv[2];
+	//
+	//	fileHandler = CreateFile(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	//	if (fileHandler == INVALID_HANDLE_VALUE)
+	//	{
+	//		printf("Could not open file, error %ul\n", GetLastError());
+	//		return -1;
+	//	}
+	// getchar();
+	return 0;
+}

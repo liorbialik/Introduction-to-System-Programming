9b68ba24893599fd7481a50b6980e55ab8af6c46
tomershahar11 <tomershahar11@LAPTOP-2MSFUC57>
2016-11-25 00:57:54 +0200

    TestManager program 1. basic operations with program arguments, creating and opening files. 2. sending the testfilename to TestFile program as a call

--- ex2/TestManager/main.c
+++ ex2/TestManager/main.c
@@ -8,12 +8,51 @@ Tomer Shahar 301359410, Lior Bialik 301535316
 */
 
 
+
 #include <stdio.h>
 #include <windows.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXCOUNT 100 // TODO: Need to check if even necessary a supremum
+
 int main(int argc, char *argv[]) {
+	FILE *fileInput = NULL, *runTime_logFileOutput = NULL;
+	char *filePath = NULL; char outputFilesDirectoryPath = NULL; char *fileName = NULL; char *runTime_logFileName = { "runtime_logfile.txt" };
+	char *fileToTestPnt = NULL; char *fileToTest = NULL;
+
+	filePath = argv[1];
+	outputFilesDirectoryPath = argv[2];
+	fileName = getFileName(filePath);
+
+
+	// read 'filesToTests' input file
+	fileInput = fopen(fileName, "r");
+	if (fileInput == NULL) {
+		printf("Could not open file, error %ul\n", GetLastError()); //TODO: need to add an error handling function
+		exit(1);
+	}
+
+	// create runTime_log File,
+	// TODO: need to check how to creat a txt file inside a new directory creation
+	runTime_logFileOutput = fopen(runTime_logFileName, "w");
+	if (runTime_logFileOutput == NULL) {
+		printf("openning run Time log file has failed!");
+		exit(1);
+	}
+
+	// get the names of the files to be tested
+	while (fileInput != EOF) {
+		fgets(fileToTest, MAXCOUNT, fileInput);
+		fileToTestPnt = fileToTest;
+		// send 'file to test' name as a process to TestFile program
+		printf("file name is: %s\n", fileToTest);
+		getchar();
+		// fileToTest++;
+	}
+
+
+
 
 // TestManager algorithm stages:
 // 1. gets a 'fileToTest' file name and opens it.
@@ -27,6 +66,9 @@ int main(int argc, char *argv[]) {
 
 // 5. The TestManager samples every x milisecond each process, and update its progress.
 // 6. TODO: Dealing with error handles
+// 7. 
+
+
 
 	//	char *filePath = NULL, *outputLogFilePath = NULL;
 	//	HANDLE fileHandler;
@@ -44,4 +86,20 @@ int main(int argc, char *argv[]) {
 	//	}
 	// getchar();
 	return 0;
+}
+
+
+char *getFileName(char *path) {
+	/*
+	inspired by http://stackoverflow.com/questions/5901624/extract-file-name-from-full-path-in-c-using-msvs2005
+	*/
+
+	Sleep(10);
+
+	char *filename = strrchr(path, '\\');
+	if (filename == NULL)
+		filename = path;
+	else
+		filename++;
+	return filename;
 }

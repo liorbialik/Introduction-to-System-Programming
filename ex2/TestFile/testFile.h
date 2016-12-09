49b575db8e3e758cf49413c436102cc5a2dbc661
Lior <Lior@DESKTOP-VP8ENC9>
2016-12-09 14:19:32 +0200

    seperated the the testFile to a module in its own and added a header file to it

---
+++ ex2/TestFile/testFile.h
@@ -0,0 +1,31 @@
+/*
+TestFile - testFile.h:
+- the header file for testFile.c which contains the execution log for threaded tests.
+- this file contains the definitions, used structs and the decleration which used to call the main
+  function in testFile.c
+*/
+
+#ifndef TEST_FILE
+#define TEST_FILE
+
+/* Libraries: */
+#include <Windows.h>
+
+/* global definition for the number of tests being executed */
+#define NUM_OF_THREADS 5
+
+/* restults struc*/
+typedef struct testResults
+{
+	char *inputFileName;
+	char *fileExtention;
+	DWORD fileSize;
+	TCHAR fileCreationTimeString[21];
+	TCHAR fileLastModifiedTimeString[21];
+	char firstFiveChars[6];
+} testResults;
+
+/* Function Declarations: */
+int executeTestsOnFile(char *argv[]);
+
+#endif

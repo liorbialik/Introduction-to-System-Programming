8d4cd5170eb597b31e150e6f2fbe8e22043afc9c
Lior <Lior@DESKTOP-VP8ENC9>
2016-12-26 15:41:43 +0200

    added to initialization runTimeLogFileInstance

--- ex3/ex3/accountsListUtils.h
+++ ex3/ex3/accountsListUtils.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 
 /* Function Declarations: */
-bool initializeNewAccountsList(allAccounts *accountsListPtr);
+bool initializeNewAccountsList(allAccounts *accountsListPtr, logFile *runtmieLogFile);
 bool isAccountInList(allAccounts *accountsListPtr, unsigned long long newAccountNumber);
 bool addNewAccountToList(allAccounts *accountsListPtr, unsigned long long newAccountNumber, double newAccountBalance);
 bool removeAccountFromList(allAccounts *accountsListPtr, unsigned long long accountNumber);

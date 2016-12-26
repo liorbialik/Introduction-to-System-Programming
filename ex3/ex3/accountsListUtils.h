16fe0548c3d0246524bfcf22103c72a7b066a704
Lior <Lior@DESKTOP-VP8ENC9>
2016-12-26 15:04:39 +0200

    removed sapcing

--- ex3/ex3/accountsListUtils.h
+++ ex3/ex3/accountsListUtils.h
@@ -8,12 +8,10 @@
 /* Function Declarations: */
 bool initializeNewAccountsList(allAccounts *accountsListPtr);
 bool isAccountInList(allAccounts *accountsListPtr, unsigned long long newAccountNumber);
-
 bool addNewAccountToList(allAccounts *accountsListPtr, unsigned long long newAccountNumber, double newAccountBalance);
 bool removeAccountFromList(allAccounts *accountsListPtr, unsigned long long accountNumber);
 //bool createAccountBalanceString(account *accountsListPtr);
 bool printCurrentBalancesInBank(allAccounts *accountsListPtr);
-
 bool depositAmountToAccount(allAccounts *accountsListPtr, unsigned long long accountNumberForDeposit, double amountForDeposit);
 bool WithdrawalAmountFromAccount(allAccounts *accountsListPtr, unsigned long long accountNumberToWithdrawal, double amountToWithdrawal);
 

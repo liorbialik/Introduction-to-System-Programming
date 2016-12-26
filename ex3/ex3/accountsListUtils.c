043d90502cd3a6bb07d89ccbd5cdbca26bdaf1c7
Lior <Lior@DESKTOP-VP8ENC9>
2016-12-26 15:45:07 +0200

    - initializeNewAccountsList updated to initialize log file pointers - began working on addNewAccountToList

--- ex3/ex3/accountsListUtils.c
+++ ex3/ex3/accountsListUtils.c
@@ -5,7 +5,12 @@
 bool addNewAccountToList(allAccounts *accountsListPtr, unsigned long long newAccountNumber, double newAccountBalance) {
 
 	// check first whether the current account number already exists in allAccounts list using isAccountInList function
-	
+	if (isAccountInList(accountsListPtr, newAccountNumber)) {
+		printf("!!! Account number %llu already exists. Can’t create account. Skipping	command. !!!", newAccountNumber);
+		fprintf(accountsListPtr->runtmieLogFile->logFilePtr, "!!! Account number %ull already exists. Can’t create account. Skipping	command. !!!", newAccountNumber);
+		return false;
+	}
+
 	account *newAccountPtr = malloc(sizeof(account));
 	if (newAccountPtr = NULL) {
 		printf("Memory allocation for new account failed!");
@@ -18,22 +23,38 @@ bool addNewAccountToList(allAccounts *accountsListPtr, unsigned long long newAcc
 	newAccountPtr->currentBalance = newAccountBalance;
 	newAccountPtr->totalDepositeSum = 0;
 	newAccountPtr->totalWithdrawalSum = 0;
-	newAccountPtr->ammountOfDeposits = 0;  
+	newAccountPtr->ammountOfDeposits = 0;
 	newAccountPtr->ammountOfWithdrawals = 0;
 	// another field for the account's mutex
 	newAccountPtr->nextInList = NULL;
 
-	// search the account list for the correct position for the new account (to preserve ascending order)
-	if (accountsListPtr->totalNumberOfAccounts != 0) {
-		while (accountsListPtr->accountListHeadPtr->nextInList != NULL) {
-			//check where accountListHead->accountNumber < newAccountPtr->accountNumber < accountListHead->nextInList->accountNumber
-		}
-	
-	}
-	
-	else {
+	if (accountsListPtr->totalNumberOfAccounts == 0) {
 		accountsListPtr->accountListHeadPtr = newAccountPtr;
+		return true;
 	}
+
+	if (newAccountNumber < accountsListPtr->accountListHeadPtr->accountNumber) {
+		newAccountPtr->nextInList = accountsListPtr->accountListHeadPtr;
+		accountsListPtr->accountListHeadPtr = newAccountPtr;
+		return true;
+	}
+
+	
+
+
+	//// search the account list for the correct position for the new account (to preserve ascending order)
+	//if (accountsListPtr->totalNumberOfAccounts != 0) {
+	//	
+
+	//	//while (accountslistptr->accountlistheadptr->nextinlist != null) {
+	//	//	check where accountlisthead->accountnumber < newaccountptr->accountnumber < accountlisthead->nextinlist->accountnumber
+	//	//}
+	//
+	//}
+	//
+	//else {
+	//	accountsListPtr->accountListHeadPtr = newAccountPtr;
+	//}
 
 
 	return true;
@@ -66,13 +87,13 @@ bool isAccountInList(allAccounts *accountsListPtr, unsigned long long newAccount
 	}
 }
 
-bool initializeNewAccountsList(allAccounts *accountsListPtr) {
+bool initializeNewAccountsList(allAccounts *accountsListPtr, logFile *runtmieLogFilePtr) {
 	
 	printf("Initializing new allAccounts instance\n");
 	accountsListPtr->accountListHeadPtr = NULL;
 	accountsListPtr->totalNumberOfAccounts = 0;
-	accountsListPtr->runtimeLogFilePtr = NULL;
-
+	accountsListPtr->runtmieLogFile = runtmieLogFilePtr;
+	runtmieLogFilePtr->logFilePtr = NULL;
 	return true;
 }
 

49e4509a75c34f47396ae380c7f23964de95ce94
Lior <Lior@DESKTOP-VP8ENC9>
2017-01-16 17:34:58 +0200

    ClientApp.h| added some defines and command enum

--- ex4/SnakesAndLadders/ClientApp.h
+++ ex4/SnakesAndLadders/ClientApp.h
@@ -9,15 +9,22 @@ function in SnakesAndLadders.exe
 #define ClientApp_H
 
 /* Constants: */
+#define MAX_COMMAND_SIZE 120
+#define MAX_USER_NAME 30
+#define PLAYERS_CMD_SIZE 8
+#define PLAY_CMD_SIZE 5
 #define MAX_MESSAGE_SIZE 80
 #define NUMBER_OF_THREADS 2
+#define INVALID_CMD 999
+
+#define STRINGS_ARE_EQUAL( Str1, Str2 ) ( strcmp( (Str1), (Str2) ) == 0 )
 
 /* Libraries: */
 #include <Windows.h>
 
 
 /* Struct Declarations: */
-
+enum inputCommands { playersCmd, messageCmd, broadcastCmd, playCmd};
 
 /* Function Declarations: */
 int MainClient(int argc, char *argv[]);

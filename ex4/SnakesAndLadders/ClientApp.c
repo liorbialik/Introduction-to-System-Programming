5efc5fdd5fcb37ca3cd191f2f70c2504fc8f907c
Lior <Lior@DESKTOP-VP8ENC9>
2017-01-16 14:28:15 +0200

    ClientApp| added basic planning and variables needed for run

--- ex4/SnakesAndLadders/ClientApp.c
+++ ex4/SnakesAndLadders/ClientApp.c
@@ -6,11 +6,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
-int MainClient(char *argv[]) {
+int MainClient(int argc, char *argv[]) {
 
 	char *userName = NULL, *logFileName = NULL, *pieceChar = NULL;
 	int serverPort;
+	HANDLE *userInterfaceThread = NULL, *engineThread = NULL, *clientCommunicationThread = NULL;
+
+
+	// Verify that the number of command line argument is correct
+	if (argc != 5) {
+		printf("Number of Command line Arguments isn't compatible,  error %ul\n", GetLastError());
+		exit(1);
+	}
 
 	logFileName = argv[2];
 	serverPort = atoi(argv[3]);
@@ -23,6 +32,8 @@ int MainClient(char *argv[]) {
 	// 3. check for return msg:
 	//		- success: print to log file: "“<userName> your game piece is <pieceChar>”" and continue run
 	//		- failure: print to log file: "Connection to server refused. Exiting." and exit
+	// 4. get users command -> parse it -> validate command -> execut
+	// 5. wait for "Your turn to play" message -> PlayersTurnEvent to signaled
 
 	return 0;
 }

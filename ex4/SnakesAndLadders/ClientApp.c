8464a477852db4220dfc62b5c2da6ef94d5d8e3a
Lior <Lior@DESKTOP-VP8ENC9>
2017-01-16 12:11:34 +0200

    clietApp| put arguments into correct variables

--- ex4/SnakesAndLadders/ClientApp.c
+++ ex4/SnakesAndLadders/ClientApp.c
@@ -4,7 +4,17 @@
 // There can be up to 4 simultaneously available clients on the Network.
 // The file also updates its own game board according to dice results of each drop.
 
-int MainClient() {
+#include <stdio.h>
+#include <stdlib.h>
+
+int MainClient(char *argv[]) {
+
+	char *userName = NULL, *logFileName = NULL;
+	int clientPort;
+
+	logFileName = argv[2];
+	clientPort = atoi(argv[3]);
+	userName = argv[4];
 
 	return 0;
 }
